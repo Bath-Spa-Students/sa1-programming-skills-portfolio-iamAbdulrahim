40f0fe3e20c8fcfc4f96f2a4efffbbf795e2de08
iamAbdulrahim <iamabdulrahim1154@gmail.com>
2024-11-06 11:48:53 +0400

    exercise 1

---
+++ exersice 1.py
@@ -0,0 +1,16 @@
+#Using the variable word one to assign the first word
+
+word_one = "Coding"
+
+#Using the variable word two to assign the second word
+
+word_two = "is"
+
+#Using the variable word three to assign the third word
+
+word_three = "cool"
+
+#String concatenation is being used to join vareiables together with giving space between them.
+#Highlighting the message "coding is cool" on the console.
+#The print statement will show the final statement.
+print(word_one + " " + word_two + " " + word_three)
