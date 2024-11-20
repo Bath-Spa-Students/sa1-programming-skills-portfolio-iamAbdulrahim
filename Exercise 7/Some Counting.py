59baaef35bd8475bdfdf691a9212a829585296ec
iamAbdulrahim <iamabdulrahim1154@gmail.com>
2024-11-20 21:49:02 +0400

    Some Counting.py

---
+++ Exercise 7/Some Counting.py
@@ -0,0 +1,24 @@
+# Counting one number at a time starting 0 to 50
+print("Counting up from 0 to 50:")
+for i in range(0, 51, 1):
+    print(i)
+
+# One number at a time, counting down from 50 to 0.
+print("\nCounting down from 50 to 0:")
+for i in range(50, -1, -1):
+    print(i)
+
+# Counting up from 30 to 50, one number at a time
+print("\nCounting up from 30 to 50:")
+for i in range(30, 51, 1):
+    print(i)
+
+# Without picking up any additional numbers, count down from 50 to 10.
+print("\n Without picking up any additional numbers, count down from 50 to 10..:")
+for i in range(50, 9, -2):
+    print(i)
+
+# Counting up from 100 to 200 while adding 5 each time
+print("\nCounting up from 100 to 200 while  adding 5 each time:")
+for i in range(100, 201, 5):
+    print(i)
